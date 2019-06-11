80f379c4433793763884bdec136eee93e462aebd
jeoy_z <jeoy_z@126.com>
2019-06-11 15:26:48 +0800

    init: init this project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="app">
+    <markdown />
+</div>
+</template>
+
+<script>
+import markdown from './components/markdown';
+
+export default {
+    name: 'App',
+    components: {
+        markdown
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/markdown.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="markdown-area">
+    <textarea class="left" type="text" v-model="text" />
+    <div class="right" v-html="html" />
+</div>
+</template>
+
+<script>
+import showdown from 'showdown';
+
+export default {
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App',
+            text: '',
+            converter: ''
+        };
+    },
+    computed: {
+        html() {
+            return this.converter.makeHtml(this.text);
+        }
+    },
+    created() {
+        this.converter = new showdown.Converter();
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.markdown-area {
+    display: flex;
+
+}
+
+.left,
+.right {
+    flex: 1;
+    height: 800px;
+}
+</style>
